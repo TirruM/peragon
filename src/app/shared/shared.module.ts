49f1cfe1f156482f2aca298782dad9db5cc07408
Thriveni-PC\Thriveni <ythriveni65@gmail.com>
2019-05-08 11:48:38 +0530

    created signin,signup and home component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SignupComponent, SigninComponent],
   imports: [
     CommonModule
   ]
