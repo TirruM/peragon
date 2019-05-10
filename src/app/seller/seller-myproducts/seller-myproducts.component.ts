5c772f311c3e7fb32dcc54bba6b0a0da3dc6e06e
Kiran <kiran@macsof.com>
2019-05-10 14:26:15 +0530

    integrate datatables module and create produts page

--- src/app/seller/seller-myproducts/seller-myproducts.component.ts
+++ src/app/seller/seller-myproducts/seller-myproducts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+declare var $;
 
 @Component({
   selector: 'app-seller-myproducts',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-myproducts.component.scss']
 })
 export class SellerMyproductsComponent implements OnInit {
-
+  @ViewChild('dataTable') table;
+  dataTable: any;
+  
   constructor() { }
 
   ngOnInit() {
+    this.dataTable = $(this.table.nativeElement);
+    this.dataTable.DataTable(
+      {
+        responsive: true
+      }
+    );
   }
 
 }
