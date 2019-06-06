59abbae79e547ac5a92fcf30ee9ec667b3b1e035
Sujatha <sujathacavusf@gmail.com>
2019-06-06 18:02:55 +0530

    UI for buyer-rfq-create component

--- src/app/buyer/buyer-rfq-create/buyer-rfq-create.component.ts
+++ src/app/buyer/buyer-rfq-create/buyer-rfq-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-buyer-rfq-create',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyerRfqCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  back() {
+    this.router.navigate(['/rfq']);
+  }
 }
