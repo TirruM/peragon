1a0adabb47f33ed3467f40ac376f5eba6d76cb7a
Thriveni-PC\Thriveni <ythriveni65@gmail.com>
2019-07-24 16:10:09 +0530

    made categories and subcategories responsive in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { PerogonServices } from "./../services/perogon-services.service";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-
+declare const $: any;
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -14,10 +14,20 @@ export class HomeComponent implements OnInit {
   productGroups: any;
   categoryArr: [];
   rowIndex: any;
+  innerRowIndex: any;
   currentCategoryObj: any;
+  mobileWidth: any;
   constructor(private router: Router, private services: PerogonServices) { }
 
   ngOnInit() {
+    console.log("mobile width" + $(window).width());
+
+    if ($(window).width() < 991) {
+      this.mobileWidth = true;
+      console.log("not desktop");
+    } else {
+      this.mobileWidth = false;
+    }
     this.categoryItems();
 
   }
@@ -28,7 +38,21 @@ export class HomeComponent implements OnInit {
     "prevArrow": "<div class='material-icons prevArrowBtn' style='position: absolute;z-index: 1000;top: 33%;left: -25px;font-size: 45px;color:#f2581d;'>arrow_back_ios</div>",
     dots: false,
     infinite: true,
-    arrows: true
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   };
 
   public categoryItems() {
@@ -65,8 +89,9 @@ export class HomeComponent implements OnInit {
     this.router.navigateByUrl("signup");
   }
 
-  onCollapsible(currentCategory, rowIndex) {
+  onCollapsible(currentCategory, rowIndex, innerRowIndex) {
     this.rowIndex = rowIndex;
+    this.innerRowIndex = innerRowIndex;
     this.currentCategoryObj = currentCategory;
   }
 }
