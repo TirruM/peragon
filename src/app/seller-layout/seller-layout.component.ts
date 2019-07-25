1dc68716a2264aa2fc36a0d448225963cd6291d9
ganesh0440 <ythriveni65@gmail.com>
2019-07-25 13:57:24 +0530

    footer component hiding, seller dashboard responsive

--- src/app/seller-layout/seller-layout.component.ts
+++ src/app/seller-layout/seller-layout.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { BroadcastserviceService } from "../services/broadcastservice.service";
 
 @Component({
   selector: 'app-seller-layout',
   templateUrl: './seller-layout.component.html',
   styleUrls: ['./seller-layout.component.scss']
 })
-export class SellerLayoutComponent implements OnInit {
+export class SellerLayoutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private broadCastService: BroadcastserviceService) { }
 
   ngOnInit() {
+    this.broadCastService.hideFooter.emit(true);
+    this.broadCastService.hideButtons.emit(true);
   }
+  ngOnDestroy(): void {
+    this.broadCastService.hideButtons.emit(false);
+    this.broadCastService.hideFooter.emit(false);
+  }
+
 
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BroadcastserviceService } from "../../services/broadcastservice.service";
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  isFooterHide: boolean = false;
+  constructor(private broadCastService: BroadcastserviceService) { }
 
   ngOnInit() {
+    this.broadCastService.hideFooter.subscribe((resp) => {
+      this.isFooterHide = resp;
+    });
   }
 
 }
