898a33bc3f4edc14f5dfee084ef87d25f683c0f1
Sujatha <sujathacavusf@gmail.com>
2019-07-29 11:47:03 +0530

    RD for create RFQ

--- src/app/buyer/buyer-bids/buyer-bids.component.ts
+++ src/app/buyer/buyer-bids/buyer-bids.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-buyer-bids',
@@ -8,9 +10,28 @@ import { Router } from '@angular/router';
 })
 export class BuyerBidsComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public breakpointObserver: BreakpointObserver) { }
+
+  isCarousel: boolean = false;
 
   ngOnInit() {
+
+    this.breakpointObserver.observe([
+      Breakpoints.Small, Breakpoints.HandsetPortrait
+    ]).subscribe((state: BreakpointState) => {
+      if(state.matches) {
+        this.isCarousel = true;
+      }
+      else {
+        this.isCarousel = false;
+      }
+    });
+
+    // BreakpointObserver.observe(['(max-width: 768px)']).subscribe(result => {
+    //   if(result.matches){
+    //     this.isCarousel = true;
+    //   }
+    // });
   }
 
   createNewRFQ() {
